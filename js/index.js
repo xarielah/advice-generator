46351af8fabc2d1bfd5470ed3a8d60d1d1bc471e
arielahr <arielah1997@gmail.com>
2023-03-17 20:45:52 +0200

    Hezi Git crash course

--- js/index.js
+++ js/index.js
@@ -1,35 +1,39 @@
+const idNode = document.querySelector("#id-advice");
+const adviceNode = document.querySelector("#body-advice");
+const btn = document.querySelector("#button");
+const heziBtn = document.querySelector("#hezi");
 
-const idNode = document.querySelector('#id-advice')
-const adviceNode = document.querySelector('#body-advice')
-const btn = document.querySelector('#button')
+const api = "https://api.adviceslip.com/advice";
 
-const api = 'https://api.adviceslip.com/advice'
-
-let canClick = true
+let canClick = true;
 
 const fetcher = async () => {
-    let response = await fetch(api)
-    let data = await response.json()
-    return data
-}
+  let response = await fetch(api);
+  let data = await response.json();
+  return data;
+};
 
 const setData = (data) => {
-    idNode.innerHTML = data.id;
-    adviceNode.innerHTML = data.advice;
-}
+  idNode.innerHTML = data.id;
+  adviceNode.innerHTML = data.advice;
+};
 
-const getAdvice = () => fetcher()
-    .then(data => setData(data.slip))
-    .catch(() => alert('Encountered an error while trying to load advices...'))
+const getAdvice = () =>
+  fetcher()
+    .then((data) => setData(data.slip))
+    .catch(() => alert("Encountered an error while trying to load advices..."));
 
-const data = getAdvice()
+const data = getAdvice();
 
-btn.addEventListener('click', () => {
-    if (!canClick) {
-        setTimeout(() => canClick = !canClick, 2000)
-        return alert('Don`t spam please ♥')
-    }
-    canClick = !canClick
-    getAdvice()
+heziBtn.addEventListener("click", () => {
+  alert("hi hezi");
+});
 
-})
+btn.addEventListener("click", () => {
+  if (!canClick) {
+    setTimeout(() => (canClick = !canClick), 2000);
+    return alert("Don`t spam please ♥");
+  }
+  canClick = !canClick;
+  getAdvice();
+});
